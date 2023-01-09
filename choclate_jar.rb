0fbfe0b22b1a9cd126e609d5a4b6e300bc4d2e2e
mankumar07 <mankumar@bestpeers.com>
2023-01-09 11:39:42 +0530

    final commit

--- choclate_jar.rb
+++ choclate_jar.rb
@@ -1,172 +1,125 @@
 $arr = []
+$remain_capacity = 0
 for i in 0..5
 puts "Press 1 for enter choclates :- "
 puts "Press 2 for Exit :- "
 puts "Press 3 for update capacity :- "
 var = gets.chomp.to_i
     if var == 1
-        $pre_capacity = 0
-        $pre_choclate = 0
+        # $pre_capacity = 0
+        # $pre_choclate = 0
+        # $remain_capacity = 0
         puts "enter capacity :- "
         $capacity = gets.chomp.to_i
         # arr1 = Array.new(capacity)
         puts "enter choclate :- "
         $choclate = gets.chomp.to_i
-
-        if $capacity > $choclate
-            if $pre_capacity > $pre_choclate
-                def func2 
-                    arr1 = []
-                    $pre_remain_capacity = $pre_capacity - $pre_choclate
-                    $len = $arr.length-1
-                    for i in 0..$pre_remain_capacity
-                        $arr[$len].push(i)
+        def fristfunc
+        # if $remain_capacity >= 1
+            if $remain_capacity <= $choclate
+                def func1 
+                    for i in 1..$remain_capacity
+                        $arr[-1].push(i)
+                    end 
+                    $choclate = $choclate - $remain_capacity
+                    if $choclate >= 1
+                        secoundfunc
                     end
-                    func1
-                end 
-                def func1
-                    arr1 = []
-                        for i in 0...$choclate
-                            arr1[i] = i+1
-                            # if capacity == i
-                            #     break
-                            # end
-                        end
-                        $pre_capacity = $capacity
-                        $pre_choclate = $choclate
-                        $arr << arr1
                 end
-                func2
-            else
+                func1
+            elsif $remain_capacity > $choclate 
+                def func1 
+                    # arr1 = []
+                    for i in 1..$choclate
+                        $arr[-1].push(i)
+                    end 
+                    $remain_capacity = $remain_capacity - $choclate
+                    # $arr << arr1
+                end
+                func1
+            end
+        end
+        def secoundfunc
+            if $capacity >= $choclate   
+                arr1 = []
+                for i in 1..$choclate
+                    arr1.push(i)
+                end
+                $remain_capacity = $capacity - $choclate
+                $arr << arr1
+            elsif $capacity < $choclate 
                 def func1
                     arr1 = []
-                    for i in 0...$choclate
-                        arr1[i] = i+1
-                        # if capacity == i
-                        #     break
-                        # end
+                    for i in 1..$capacity
+                        arr1.push(i)
                     end
-                    $pre_capacity = $capacity
-                    $pre_choclate = $choclate
-                    puts $pre_capacity = $capacity,$pre_choclate = $choclate
+                    $arr << arr1
+                    $capacity = $choclate - $capacity
+                    if $capacity >= 1
+                        func2
+                    end
+                end
+                def func2
+                    arr1 = []
+                    for i in 1..$capacity
+                        arr1.push(i)
+                    end
                     $arr << arr1
                 end
-            func1
-            end
-        
-        elsif $capacity < $choclate
-            if $pre_capacity > $pre_choclate
-                puts "helloooooooooooooooooo"
-                def func2 
-                    arr1 = []
-                    $pre_remain_capacity = $pre_capacity - $pre_choclate
-                    $len = $arr.length-1
-                    if $pre_remain_capacity > $choclate
-                        for i in 0..$choclate
-                            $arr[$len].push(i)
-                        end
-                    else
-                        for i in 0..$pre_remain_capacity
-                            $arr[$len].push(i)
-                        end
-                    end
-                    func1
-                end 
-                def func1
-                    arr1 = []
-                    for i in 0..$capacity-1
-                        arr1[i] = i+1
-                        if i == $capacity-1
-                            $arr << arr1
-                            $pre_capacity = $capacity
-                            $pre_choclate = $choclate
-                            $choclate = $choclate-arr1[i]
-                            $capacity = $choclate
-                            # func1
-                        end
-                    end
-                end
-                func2
-            else    
-                def func1
-                    arr1 = []
-                    for i in 0..$capacity-1
-                        arr1[i] = i+1
-                        if i == $capacity-1
-                            $arr << arr1
-                            $pre_capacity = $capacity
-                            $pre_choclate = $choclate
-                            $choclate = $choclate-arr1[i]
-                            $capacity = $choclate
-                            func1
-                        end
-                    end
-                end
                 func1
             end
-        else
-            if $pre_capacity > $pre_choclate
-                def fun2 
-                    arr1 = []
-                    $pre_remain_capacity = $pre_capacity - $pre_choclate
-                    $len = $arr.length-1
-                    for i in 0..$pre_remain_capacity
-                        $arr[$len].push(i)
-                    end
-                    func1
-                end 
-                def func1
-                    arr1 = []
-                        for i in 0...$choclate
-                            arr1[i] = i+1
-                            # if capacity == i
-                            #     break
-                            # end
-                        end
-                        $pre_capacity = $capacity
-                        $pre_choclate = $choclate
-                        $arr << arr1
-                end
-            func2
-            else
-            #     def func1
-            #         arr1 = []
-            #         for i in 0...$choclate
-            #             arr1[i] = i+1
-            #             # if capacity == i
-            #             #     break
-            #             # end
-            #         end
-            #         $pre_capacity = $capacity
-            #         $pre_choclate = $choclate
-            #         $arr << arr1
-            #     end
-            # func1
-                def func1
-                    arr1 = []
-                    for i in 0..$capacity-1
-                        arr1[i] = i+1
-                        if i == $capacity-1
-                            $arr << arr1
-                            $pre_capacity = $capacity
-                            $pre_choclate = $choclate
-                            $choclate = $choclate-arr1[i]
-                            $capacity = $choclate
-                            func1
-                        end
-                    end
-                end
-                func1
-            end
-            redo
         end
+        if $remain_capacity >= 1
+            fristfunc
+        else
+            secoundfunc
+        end
+        redo
     elsif var == 2
         puts "Your level is #{$arr.length}"
-        puts $pre_capacity = $capacity,$pre_choclate = $choclate
-        print $arr
+        # puts $remain_capacity
+        # print $arr
 
         break
     elsif var == 3
-        puts "this is 3 part"
+        puts "Enter level you want update :- "
+        $level = gets.chomp.to_i
+        puts "Enter capacity you want update :- "
+        $updatecapcity = gets.chomp.to_i
+
+        # ----------------------------------------------------------------------------
+        if $arr[$level-1].length < $updatecapcity
+            var1 = $updatecapcity - $arr[$level-1].length
+            arr2 = $arr[-1].last(var1)
+            for i in arr2
+                $arr[$level-1] << i
+            end
+            if $arr[-1].length == var1
+                $arr.delete($arr[-1])
+            else
+                $arr[-1].shift(var1)
+            end
+            puts "Your capacity is updated !!!!!!"
+        elsif $arr[$level-1].length > $updatecapcity
+            var1 = $arr[$level-1].length - $updatecapcity
+            $arr[$level-1].shift(var1)
+            if $remain_capacity >= var1
+                    for i in 1..var1
+                        $arr[-1] << i
+                    end 
+            else 
+                for i in 1..$remain_capacity
+                    $arr[-1] << i 
+                end
+                var1 = var1 - $remain_capacity
+                arr1 = []
+                for i in 1..var1
+                    arr1.push(i)
+                end
+                $arr << arr1
+                $remain_capacity = 0
+            end 
+            puts "Your capacity is updated !!!!!!"
+        end
     end
 end
